925332f658bde2d9794490c6bb4062015abebbd6
emtl688 <ericpmoran1@gmail.com>
2023-02-13 15:47:54 -0500

    config setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    App
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <main>
+        <Switch>
+          <Route path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route path="/movies">
+            <h1>Movies</h1>
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
 
 export default App;
